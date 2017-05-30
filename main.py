00191ff5fb86be12558b184a0bf8c4fae49e2f20
Luke Samaritan <sam4ritan@users.noreply.github.com>
2017-05-30 16:05:30 +0200

    Create main.py
    created it. Variables are named in german. Komm damit klar.

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+import random
+import itertools
+
+zeitweise=[]
+ausgabe=[]
+
+for h in range(len(sys.argv)):
+
+    if h > 0:
+        wort = list(sys.argv[h])
+
+        for i in range(len(wort)):
+            tmp = random.random()
+            if tmp >= 0.5: 
+                wort[i]=wort[i].lower()
+            else:
+                wort[i]=wort[i].upper()
+        zeitweise.append(wort)
+
+ausgabe=list(itertools.chain.from_iterable(zeitweise))
+print(''.join(ausgabe))
